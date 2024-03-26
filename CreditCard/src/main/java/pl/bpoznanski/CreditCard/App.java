fcd01451d890061e95f5f36a963f1ec5a620a4ef
BartlomiejPoznanski <114911932+BartlomiejPoznanski@users.noreply.github.com>
2024-03-26 17:01:10 +0100

    Delete CreditCard directory

--- CreditCard/src/main/java/pl/bpoznanski/CreditCard/App.java
+++
@@ -1,7 +0,0 @@
-package pl.bpoznanski.CreditCard;
-
-public class App {
-    public static void main(String[] args){
-
-    }
-}
--- CreditCard/src/main/java/pl/bpoznanski/CreditCard/CreditBelowThresholdException.java
+++
@@ -1,5 +0,0 @@
-package pl.bpoznanski.CreditCard;
-
-public class CreditBelowThresholdException extends CreditCard {
-
-}
